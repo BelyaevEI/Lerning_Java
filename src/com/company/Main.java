49a8e4faec48db0cd4e9e88513556422a44f93d4
egorb <egor.belyaev95@yandex.ru>
2021-08-26 21:13:30 +0300

    Work with input and switch.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число :");
+        int num = input.nextInt();
+
+        switch (num) {
+            case 3:
+                System.out.print("Число равно трем");
+                break;
+            case 5:
+                System.out.print("Число равно пяти");
+                break;
+            default:
+                System.out.print("Что-то другое");
+        }
     }
 }
