3c9aff7dc2030883070e6c8845172b479d728bea
egorb <egor.belyaev95@yandex.ru>
2021-08-26 22:21:38 +0300

    Task #1

--- src/com/company/Cycle.java
+++ src/com/company/Cycle.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Cycle {
     public static void main(String[] args) {
         for( int i = 10; i < 21; i++){
-            System.out.println("Квадрат числа " + i + ":");
-            System.out.println(Math.pow(i, 2));
+            System.out.printf("Квадрат числа %d равен %.0f \n", i, Math.pow(i, 2) );
+//            System.out.println(Math.pow(i, 2));
         }
     }
 }
